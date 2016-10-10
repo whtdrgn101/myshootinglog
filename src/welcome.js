e588979cd286e341ca84ee5b43addee9116b52df
Tim DeWees <timothy.dewees@gmail.com>
2016-10-10 14:56:17 -0400

    Updated stats logic

--- src/welcome.js
+++ src/welcome.js
@@ -2,6 +2,8 @@ import {computedFrom, inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import Store from 'common/shooting-log-store';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import environment from 'environment';
+import {HttpClient} from 'aurelia-http-client';
 
 @inject(Router, Store, EventAggregator)
 export class Welcome {
@@ -13,19 +15,52 @@ export class Welcome {
     this.router = router;
     this.store = store;
     this.eventAggregator = eventAggregator;
+    this.client = new HttpClient().configure(x => {
+      x.withBaseUrl(environment.API_URL);
+      x.withHeader('x-authorization',this.store.authToken.token);
+    });
   }
 
   activate(parms, routeConfig) {
     this.eventAggregator.publish('viewActivate');
-    var self = this;
-    this.store.getLast30Stats().then(function(stats){
-      self.last30Stats = stats[0];
-    });
-    this.store.getLifetimeStats().then(function(stats){
-      self.lifetimeStats = stats[0];
-    });
+    this.getLifetimeStats();
+    this.getLast30Stats();
+
   }
 
+  getLifetimeStats() {
+    var self = this;
+    this.client.get("/stats/" + this.store.authToken.userId + "/lifetime")
+      .then(data => {
+        self.lifetimeStats = JSON.parse(data.response);
+      })
+      .catch(error => {
+        if(error.status === 403) {
+          alert('Session timeout, please log in again');
+          this.router.navigateToRoute('login');
+        } else {
+          this.error = "An error occurred while retrieving the job list, please contact support or try again";
+        }
+
+      });
+  }
+
+  getLast30Stats() {
+    var self = this;
+    this.client.get("/stats/" + this.store.authToken.userId + "/last30")
+      .then(data => {
+        self.last30Stats = JSON.parse(data.response);
+      })
+      .catch(error => {
+        if(error.status === 403) {
+          alert('Session timeout, please log in again');
+          this.router.navigateToRoute('login');
+        } else {
+          this.error = "An error occurred while retrieving the job list, please contact support or try again";
+        }
+
+      });
+  }
   get latestRound() {
 
   }
