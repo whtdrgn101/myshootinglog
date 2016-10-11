c79bdb5933bd223d1a98d5bc23a9fa0fb10b5fa6
Ubuntu <timothy.dewees@gmail.com>
2016-10-11 12:41:46 +0000

    Updated user profile and cleaned out uneeded code

--- src/common/shooting-log-store.js
+++ src/common/shooting-log-store.js
@@ -1,15 +1,11 @@
 import {inject} from 'aurelia-framework';
-import {HttpClient} from 'aurelia-fetch-client';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import {HttpClient} from 'aurelia-fetch-client';
 
 @inject(EventAggregator, HttpClient)
 export default class ShootingLogStore {
 
   user = null;
-  scorecard = null;
-  errorMessage = null;
-  round = null;
-  bow = null;
   authToken = null;
 
   constructor(_eventAggregator, _http){
@@ -29,40 +25,11 @@ export default class ShootingLogStore {
 
   subscribe(){
     let self = this;
-    this.eventAggregator.subscribe('scorecard.changed', payload => {
-      self.handleScorecardChanged();
-    });
     this.eventAggregator.subscribe('logout', payload => {
       self.handleLogout();
     });
     this.eventAggregator.subscribe('viewActivate', payload => {
       self.handleViewActivate();
-    });
-    //Bow handlers
-    this.eventAggregator.subscribe('bow.add', payload => {
-      self.handleBowAdd(payload);
-    });
-    this.eventAggregator.subscribe('bow.update', payload => {
-      self.handleBowUpdate(payload);
-    });
-    this.eventAggregator.subscribe('bow.delete', payload => {
-      self.handleBowDelete(payload);
-    });
-    //Scorecard
-    this.eventAggregator.subscribe('round.add', payload => {
-      self.handleRoundAdd(payload);
-    });
-    this.eventAggregator.subscribe('round.delete', payload => {
-      self.handleRoundDelete(payload);
-    });
-    this.eventAggregator.subscribe('round.update', payload => {
-      self.handleRoundUpdate(payload);
-    });
-    this.eventAggregator.subscribe('bowStore.stale', payload => {
-      self.handleBowStoreStale(payload);
-    });
-    this.eventAggregator.subscribe('roundStore.stale', payload => {
-      self.handleRoundStoreStale(payload);
     });
     this.eventAggregator.subscribe('user.authenticated', payload => {
       self.handleUserAuthenticated(payload);
@@ -89,178 +56,6 @@ export default class ShootingLogStore {
     this.authToken = null;
   }
   
-  handleScorecardChanged() {
-    var user = this.getUserInSession();
-    var self = this;
-    //Hack to make mongoose behave.  Need to clean this up at the API side
-    this.scorecard._id = undefined;
-    this.scorecard.id = undefined;
-    return new Promise(function (resolve, reject) {
-      self.http.fetch('scorecard/' + user.userId, {
-        method: 'put',
-        body: JSON.stringify(self.scorecard)
-      }).then(function(results) {
-        resolve(results);
-      })
-    });
-  }
-  
-  /************************
-   * ROUND EVENT HANDLERS *
-   ************************/
-  handleRoundDelete(roundId) {
-    var user = this.getUserInSession();
-    var self = this;
-    this.http.fetch('scorecard/' + user.userId + '/rounds/' + roundId, {method: 'delete'})
-      .then(function(results) {
-        self.eventAggregator.publish('roundStore.stale');
-      });
-  }
-  
-  handleRoundAdd(round) {
-    var user = this.getUserInSession();
-    var self = this;
-    this.http.fetch('scorecard/' + user.userId + '/rounds', {
-      method: 'post',
-      body: JSON.stringify(round)
-    }).then(function(results) {
-        self.eventAggregator.publish('roundStore.stale');
-      });
-  }
-  
-  handleRoundUpdate(round) {
-    var user = this.getUserInSession();
-    var self = this;
-    this.http.fetch('scorecard/' + user.userId + '/rounds/' + round._id, {
-      method: 'put',
-      body: JSON.stringify(round)
-    }).then(function(results) {
-        self.eventAggregator.publish('roundStore.stale');
-      });
-  }
-  
-  handleRoundStoreStale() {
-    var user = this.getUserInSession();
-    var self = this;
-    this.http.fetch('scorecard/' + user.userId + '/rounds')
-      .then(response => response.json())
-      .then(function(rounds) {
-        self.scorecard.rounds = rounds;
-        });
-  }
-  
-  /**********************
-   * BOW EVENT HANDLERS *
-   **********************/
-  handleBowAdd(bow) {
-    var user = this.getUserInSession();
-    var self = this;
-    this.http.fetch('scorecard/' + user.userId + '/bows', {
-      method: 'post',
-      body: JSON.stringify(bow)
-    }).then(function(results) {
-        self.eventAggregator.publish('bowStore.stale');
-      });
-  }
-
-  handleBowUpdate(bow) {
-    var user = this.getUserInSession();
-    var self = this;
-    this.http.fetch('scorecard/' + user.userId + '/bows/' + bow._id, {
-      method: 'put',
-      body: JSON.stringify(bow)
-    }).then(function(results) {
-        self.eventAggregator.publish('bowStore.stale');
-      });
-  }
-
-  handleBowDelete(bowId) {
-    var user = this.getUserInSession();
-    var self = this;
-    this.http.fetch('scorecard/' + user.userId + '/bows/' + bowId, {method: 'delete'})
-      .then(function(results) {
-        self.eventAggregator.publish('bowStore.stale');
-      });
-  }
-  
-  handleBowStoreStale() {
-    var user = this.getUserInSession();
-    var self = this;
-    this.http.fetch('scorecard/' + user.userId + '/bows')
-      .then(response => response.json())
-      .then(function(bows) {
-        self.scorecard.bows = bows;
-      });
-  }
-  
-  /***********
-   * METHODS *
-   ***********/
-  getScorecard(userId) {
-    var self = this;
-    return new Promise(function (resolve, reject) {
-      if(self.scorecard == null || self.scorecard == undefined) {
-        
-        self.http.fetch('scorecard/'+ userId)
-        .then(response => response.json())
-        .then(function(results) {
-          resolve(results);
-        })
-        
-      } else {
-        resolve(self.scorecard);
-      }
-    });
-  }
-
-  getBow(bowId) {
-    var user = this.getUserInSession();
-    var self = this;
-    return new Promise(function (resolve, reject) {
-      self.http.fetch('scorecard/' + user.userId + '/bows/' + bowId)
-      .then(response => response.json())
-      .then(function(bow) {
-        resolve(bow);
-        });
-    });
-  }
-  
-  getRound(roundId) {
-    var user = this.getUserInSession();
-    var self = this;
-    return new Promise(function (resolve, reject) {
-      self.http.fetch('scorecard/' + user.userId + '/rounds/' + roundId)
-      .then(response => response.json())
-      .then(function(round) {
-        resolve(round);
-        });
-    });
-  }
-
-  getLast30Stats() {
-    var user = this.getUserInSession();
-    var self = this;
-    return new Promise(function (resolve, reject) {
-      self.http.fetch('stats/' + user.userId + '/last30')
-        .then(response => response.json())
-        .then(function(round) {
-          resolve(round);
-        });
-    });
-  }
-
-  getLifetimeStats() {
-    var user = this.getUserInSession();
-    var self = this;
-    return new Promise(function (resolve, reject) {
-      self.http.fetch('stats/' + user.userId + '/lifetime')
-        .then(response => response.json())
-        .then(function(round) {
-          resolve(round);
-        });
-    });
-  }
-
   /*********************
    *       USER
    *********************/
@@ -299,24 +94,7 @@ export default class ShootingLogStore {
   }
 
   /**
-   * TODO: Kill this stupid method!
-   */
-  getUserDetails(user) {
-    let userDetails = {
-      name: '',
-      location: '',
-      image: ''
-    };
-    if(this.scorecard.user.userId === user.userId) {
-      userDetails.name =  this.scorecard.user.details.name;
-      userDetails.location =  this.scorecard.user.details.location;
-      userDetails.image =  this.scorecard.user.details.image;
-    }
-    return userDetails;
-  }
-
-  /**
-   * Created the Firebase account and returns the accountID which will be the key
+   * Created the user account and returns the accountID which will be the key
    * the API uses for accessing the users data
    */
   register(_email, _password) {
@@ -333,30 +111,5 @@ export default class ShootingLogStore {
       });
     });
   }
-  
-  /**
-   * After registration, the users scorecard must be initialized by POSTing a 
-   * stub record to the API.  
-   */
-  initUserScorecard(user) {
-    var card = {
-      user: {
-        accountId: user._id,
-        email: user.email
-      },
-      rounds: [],
-      bows: []
-    }
-    var self = this;
-    return new Promise(function (resolve, reject) {
-      self.http.fetch('scorecard', {
-        method: 'post',
-        body: JSON.stringify(card)
-      }).then(function(results) {
-        resolve(results);
-      })
-    });
-  }
-  
 
 }
--- src/user/profile.js
+++ src/user/profile.js
@@ -2,24 +2,61 @@ import {computedFrom, inject, URL} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import ShootingLogStore from 'common/shooting-log-store';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import environment from '../environment';
+import {HttpClient} from 'aurelia-http-client';
 
 @inject(Router, ShootingLogStore, EventAggregator)
 export class Profile {
 
   file;
-
+  user;
+  
   constructor(router, shootingLogStore, eventAggregator) {
     this.appRouter = router;
     this.store = shootingLogStore;
     this.dispatcher = eventAggregator;
+    this.client = new HttpClient().configure(x => {
+      x.withBaseUrl(environment.API_URL);
+      x.withHeader('x-authorization',this.store.authToken.token);
+    });
   }
 
   activate() {
     this.dispatcher.publish('viewActivate');
+    var self = this;
+    this.client.get("/user/" + this.store.authToken.userId )
+      .then(data => {
+        self.user = JSON.parse(data.response);
+      })
+      .catch(error => {
+        if(error.status === 403) {
+          alert('Session timeout, please log in again');
+          this.store.clearStore();
+          this.router.navigateToRoute('login');
+        } else {
+          this.error = "An error occurred while retrieving the job list, please contact support or try again";
+        }
+
+      });
+    
   }
 
   saveProfile() {
-    this.dispatcher.publish('scorecard.changed');
+    var self = this;
+    this.client.put("/user/" + this.store.authToken.userId, JSON.stringify(this.user) )
+      .then(data => {
+        self.user = JSON.parse(data.response);
+      })
+      .catch(error => {
+        if(error.status === 403) {
+          alert('Session timeout, please log in again');
+          this.store.clearStore();
+          this.router.navigateToRoute('login');
+        } else {
+          this.error = "An error occurred while retrieving the job list, please contact support or try again";
+        }
+
+      });
   }
 
   embedFile() {
@@ -28,7 +65,7 @@ export class Profile {
       var reader = new FileReader();
       reader.onload = (function(theFile) {
         return function(e) {
-          self.store.user.image = e.target.result;
+          self.user.profile_pic = e.target.result;
         };
       })(this.file[0]);
       reader.readAsDataURL(this.file[0]);
